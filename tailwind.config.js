45f47ce214c796d60f9b35d6ce2b65cb66d83693
Byenna <72694655+Byenna@users.noreply.github.com>
2022-10-09 12:03:55 +0200

    68 Customizing Container

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
   theme: {
+    screens : {
+      sm : '576px',
+      md : '768px',
+      lg : '992px',
+      xl : '1200px',
+    },
+    container : {
+      center : true,
+      padding : '1rem' // 16px
+    },
     extend: {
       fontFamily: {
       'montserrat' : ['Montserrat'],
